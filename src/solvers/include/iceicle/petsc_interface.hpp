fbf7644fea168ec45f641e017a8180126ae464ec
gabsillis <gabsill@ncsu.edu>
2024-02-16 00:15:17 -0500

    petsc lightweight interface

--- src/solvers/include/iceicle/petsc_interface.hpp
+++ src/solvers/include/iceicle/petsc_interface.hpp
@@ -1,1 +1,177 @@
+#pragma once
+#include <mpi.h>
 #include <petsc.h>
+#include <petscerror.h>
+#include <mdspan/mdspan.hpp>
+#include <petscsystypes.h>
+#include <petscvec.h>
+
+namespace ICEICLE::PETSC {
+
+    /**
+     * @brief add to a logically dense block from an mdspan to a pestc matrix A 
+     * Adds the values, if there previously was no value, just puts the value in that location
+     * @param A the petsc matrix to add values to
+     * @param rowstart the global starting row index of the block 
+     * @param colstart the global starting column index of the block 
+     * @param data the logically dense matrix to add 
+     * @param comm the mpi communicator
+     */
+    template<typename T, class Extents, class LayoutPolicy, class AccessorPolicy>
+    void add_to_petsc_mat(
+        Mat A,
+        std::size_t rowstart,
+        std::size_t colstart,
+        std::experimental::mdspan<T, Extents, LayoutPolicy, AccessorPolicy> data,
+        MPI_Comm comm = MPI_COMM_WORLD
+    ) noexcept {
+        std::size_t dimprod = data.extent(0) * data.extent(1);
+        // indices are the offest plus 0, 1, ... for rows and columns
+        // row idxm[i] and column idxn[j] set the value values[i*n + j]
+        std::vector<PetscInt> idxm(data.extent(0));
+        std::vector<PetscInt> idxn(data.extent(1));
+        std::iota(idxm.begin(), idxm.end(), rowstart);
+        std::iota(idxn.begin(), idxn.end(), colstart);
+
+        std::vector<T> values{};
+        values.reserve(dimprod);
+        for(std::size_t i = 0; i < data.extent(0); ++i){
+            for(std::size_t j = 0; j < data.extent(1); ++j){
+                values.push_back(data[i, j]);
+            }
+        }
+        PetscCallAbort(comm, MatSetValues(A, data.extent(0), idxm.data(), data.extent(1),
+                    idxn.data(), values.data(), ADD_VALUES));
+    }
+
+
+    /**
+     * @brief insert a logically dense block from an mdspan to a pestc matrix A 
+     * inserts the values or overwrites previous values
+     * @param A the petsc matrix to add values to
+     * @param rowstart the global starting row index of the block 
+     * @param colstart the global starting column index of the block 
+     * @param data the logically dense matrix to insert
+     * @param comm the mpi communicator
+     */
+    template<typename T, class Extents, class LayoutPolicy, class AccessorPolicy>
+    void insert_in_petsc_mat(
+        Mat A,
+        std::size_t rowstart,
+        std::size_t colstart,
+        std::experimental::mdspan<T, Extents, LayoutPolicy, AccessorPolicy> data,
+        MPI_Comm comm = MPI_COMM_WORLD
+    ) noexcept {
+        std::size_t dimprod = data.extent(0) * data.extent(1);
+        std::vector<PetscInt> idxm{};
+        std::vector<PetscInt> idxn{};
+        std::vector<T> values{};
+        idxm.reserve(dimprod);
+        idxn.reserve(dimprod);
+        values.reserve(dimprod);
+        for(std::size_t i = 0; i < data.extent(0); ++i){
+            for(std::size_t j = 0; j < data.extent(1); ++j){
+                idxm.push_back(i + rowstart);
+                idxn.push_back(j + colstart);
+                values.push_back(data[i, j]);
+            }
+        }
+        PetscCallAbort(comm, MatSetValues(A, data.extent(0), idxm.data(),data.extent(1),
+                    idxn.data(), data.data(), values.data(), INSERT_VALUES));
+    }
+
+    /**
+     * @brief a non-owning view of a petsc vec 
+     * automatically releases view with destructor or the release() function
+     */
+    class VecSpan {
+        Vec v;
+        PetscScalar *_data;
+        PetscScalar *_end_data;
+
+        public:
+
+        using iterator = PetscScalar*;
+        using reference = PetscScalar&;
+        using pointer = PetscScalar*;
+        using size_type = std::size_t;
+
+        /**
+         * @brief create a span from a Petsc vec 
+         */
+        inline VecSpan(Vec v) : v(v) {
+            PetscInt local_size;
+            VecGetLocalSize(v, &local_size);
+            VecGetArray(v, &_data);
+            _end_data = _data + local_size;
+        }
+
+        // delete copy semantics
+        VecSpan(const VecSpan &other) = delete;
+        VecSpan &operator=(const VecSpan &other) = delete;
+
+        VecSpan(VecSpan &&other) = default;
+        VecSpan &operator=(VecSpan &&other) = default;
+
+        /**
+         * @brief release this view 
+         * afterwards, this will non longer function to access the vec 
+         * WARNING: this container is invalid after release()
+         * NOTE: A preferred pattern may be to use this in a scoped section of code 
+         * and allow the destructor to release the view at the end of the sub-scope
+         */
+        inline void release(){
+            VecRestoreArray(v, &_data);
+            _data = nullptr;
+            _end_data = nullptr;
+        }
+
+        /// @brief destructor
+        ~VecSpan(){
+            if(_data != nullptr) release();
+        }
+
+        /// @brief returns an iterator to the first element of the span,
+        /// if the span is empty, the returned iterator will be equal to end 
+        inline constexpr iterator begin() const noexcept { return _data; }
+
+        /// @brief returns an iterator to the first element of the span,
+        /// if the span is empty, the returned iterator will be equal to end 
+        inline constexpr const iterator cbegin() const noexcept { return _data; }
+
+        /// @brief 
+        /// Returns an iterator to the element following the last element of the span.
+        /// This element acts as a placeholder; attempting to access it results in undefined behavior. 
+        inline constexpr iterator end() const noexcept { return _end_data; } 
+
+        /// @brief 
+        /// Returns an iterator to the element following the last element of the span.
+        /// This element acts as a placeholder; attempting to access it results in undefined behavior. 
+        inline constexpr const iterator cend() const noexcept { return _end_data; } 
+
+        /// @brief returns a reverse iterator to the first element of the reversed span
+        inline constexpr iterator rbegin() const noexcept { return _end_data - 1; }
+
+        /// @brief returns a reverse iterator to the first element of the reversed span
+        inline constexpr iterator const crbegin() const noexcept { return _end_data - 1; }
+
+        /// @brief returns a reverse iterator the last element of the reversed span
+        inline constexpr iterator rend() const noexcept { return _data - 1; }
+
+        /// @brief returns a reverse iterator the last element of the reversed span
+        inline constexpr iterator const crend() const noexcept { return _data - 1; }
+
+        /// @brief returns a reference to the first element in the span
+        inline constexpr reference front() const { return _data[0]; }
+
+        /// @brief returns a reference to the last element in the span
+        inline constexpr reference back() const { return _end_data[-1]; }
+
+        /// @brief returns a reference to the idxth element in the _data 
+        inline constexpr reference operator[](size_type idx) const { return _data[idx]; } 
+
+        /// @brief get the underlying pointer 
+        inline constexpr pointer data() const { return _data; }
+    };
+
+}
--- test/test_hypre_interface.cpp
+++ test/test_hypre_interface.cpp
@@ -5,6 +5,10 @@
 #include "iceicle/petsc_interface.hpp"
 #include "mdspan/mdspan.hpp"
 #include "gtest/gtest.h"
+#include <petscksp.h>
+#include <petscmat.h>
+#include <petscvec.h>
+#include <petscviewer.h>
 
 TEST(test_hypre_interface, test_bounds){
     using T = double;
@@ -95,8 +99,10 @@ TEST(test_hypre_interface, test_hypre_vec){
 
 namespace stdex = std::experimental;
 
-TEST(test_hypre_interface, test_hypre_mat){
-    using namespace ICEICLE::SOLVERS;
+TEST(test_petsc_interface, test_matrix_utils){
+    /// WARNING: FAILS WITH DEFAULT ILU PRECONDITIONER
+    /// Probably because of zero diagonals that don't get resolved
+    using namespace ICEICLE::PETSC;
 
     auto comm = MPI_COMM_WORLD;
     // MPI info
@@ -104,29 +110,33 @@ TEST(test_hypre_interface, test_hypre_mat){
     int proc_id;
     MPI_Comm_size(comm, &nproc);
     MPI_Comm_rank(comm, &proc_id);
-    HYPRE_BigInt sz = 6;
-    HypreBounds mat_bounds{.ilower = sz * proc_id, .iupper = sz * (proc_id + 1) - 1};
-    HypreMat hmat{mat_bounds};
+
+    // create a 6x6 matrix on each processor
+    Mat A;
+    MatCreate(comm, &A);
+    MatSetSizes(A, 6, 6, PETSC_DETERMINE, PETSC_DETERMINE);
+    MatSetFromOptions(A);
+
+    PetscInt global_index_start = 6 * proc_id;
 
     std::array vals1{
-        0.0, 0.5, 1.0,
+        0.0, 5.0, 1.0,
         3.0, 3.0, 4.0,
-        2.0, 7.0, 6.0
+        2.0, 7.0, 6.0,
     };
 
     stdex::mdspan arr1{vals1.data(), stdex::extents{3, 3}};
 
     std::array vals2 {
-        2.0, 1.0,
-        7.0, 2.0,
-        1.0, 4.0
+        2.0, 1.0, 0.0, 0.0,
+        7.0, 2.0, 0.0, 0.0,
+        1.0, 4.0, 0.0, 0.0
     };
-    stdex::mdspan arr2{vals2.data(), stdex::extents{3, 2}};
+    stdex::mdspan arr2{vals2.data(), stdex::extents{3, 4}};
 
-    hmat.set_values(0, 0, arr1);
-    hmat.add_values(2, 1, arr2);
-    hmat.add_values(3, 3, arr1);
-    HYPRE_IJMatrixAssemble(hmat);
+    add_to_petsc_mat(A, global_index_start + 0, global_index_start + 0, arr1);
+    add_to_petsc_mat(A, global_index_start + 2, global_index_start + 1, arr2);
+    add_to_petsc_mat(A, global_index_start + 3, global_index_start + 3, arr1);
 // A =
 //
 //     0     5     1     0     0     0
@@ -136,73 +146,55 @@ TEST(test_hypre_interface, test_hypre_mat){
 //     0     1     4     3     3     4
 //     0     0     0     2     7     6
 
-    HypreVec xvec{mat_bounds};
-    HypreVec bvec{mat_bounds};
+    MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY);
 
-    std::array bvals{
-        1.0, 2.0, 2.0, 1.0, 3.0, 4.0
-    };
-    bvec.set_values(0, std::span{bvals});
+    MatView(A, PETSC_VIEWER_STDOUT_WORLD);
+    
+    KSP ksp;
+    PetscCallAbort(comm, KSPCreate(comm, &ksp));
+    PetscCallAbort(comm, KSPSetOperators(ksp, A, A));
 
-    HYPRE_IJMatrixPrint(hmat, "test_matrix");
+    Vec x, b;
+    VecCreate(comm, &x);
+    VecSetSizes(x, 6, PETSC_DETERMINE);
+    VecSetFromOptions(x);
+    PetscCallAbort(comm, VecDuplicate(x, &b));
 
-    HYPRE_Solver solver, precond;
-    /* Run info - needed logging turned on */
-    int num_iterations;
-    double final_res_norm;
-    int restart = 30;
-      /* Create solver */
-      HYPRE_ParCSRFlexGMRESCreate(MPI_COMM_WORLD, &solver);
-
-      /* Set some parameters (See Reference Manual for more parameters) */
-      HYPRE_FlexGMRESSetKDim(solver, restart);
-      HYPRE_FlexGMRESSetMaxIter(solver, 5000); /* max iterations */
-      HYPRE_FlexGMRESSetTol(solver, 1e-7); /* conv. tolerance */
-      HYPRE_FlexGMRESSetPrintLevel(solver, 2); /* print solve info */
-      HYPRE_FlexGMRESSetLogging(solver, 1); /* needed to get run info later */
-
-
-      /* Now set up the AMG preconditioner and specify any parameters */
-      HYPRE_BoomerAMGCreate(&precond);
-      HYPRE_BoomerAMGSetPrintLevel(precond, 1); /* print amg solution info */
-      HYPRE_BoomerAMGSetCoarsenType(precond, 1);
-      HYPRE_BoomerAMGSetOldDefault(precond);
-      HYPRE_BoomerAMGSetRelaxType(precond, 0); /* Jacobi */
-      HYPRE_BoomerAMGSetNumSweeps(precond, 1);
-      HYPRE_BoomerAMGSetTol(precond, 0.0); /* conv. tolerance zero */
-      HYPRE_BoomerAMGSetMaxIter(precond, 1); /* do only one iteration! */
-
-      /* Set the FlexGMRES preconditioner */
-      HYPRE_FlexGMRESSetPrecond(solver, (HYPRE_PtrToSolverFcn) HYPRE_BoomerAMGSolve,
-                                (HYPRE_PtrToSolverFcn) HYPRE_BoomerAMGSetup, precond);
-
-      /* Now setup and solve! */
-      HYPRE_ParCSRFlexGMRESSetup(solver, hmat, bvec, xvec);
-      HYPRE_ParCSRFlexGMRESSolve(solver, hmat, bvec, xvec);
-
-      /* Run info - needed logging turned on */
-      HYPRE_FlexGMRESGetNumIterations(solver, &num_iterations);
-      HYPRE_FlexGMRESGetFinalRelativeResidualNorm(solver, &final_res_norm);
-
-
-    if (proc_id == 0)
+    // Fill the b vector values 
     {
-        printf("\n");
-        printf("Iterations = %d\n", num_iterations);
-        printf("Final Relative Residual Norm = %e\n", final_res_norm);
-        printf("\n");
+        VecSpan b_view{b};
+        b_view[0] = 1.0;
+        b_view[1] = 2.0;
+        b_view[2] = 2.0;
+        b_view[3] = 1.0;
+        b_view[4] = 3.0;
+        b_view[5] = 4.0;
+    } // end scope to relinquish view
+    VecSet(x, 0);
+
+    // default to jacobi preconditioner 
+    PC pc;
+    PetscCallAbort(comm, KSPGetPC(ksp, &pc));
+    PCSetType(pc, PCJACOBI);
+
+    /// the user can still override
+    PetscCallAbort(comm, KSPSetFromOptions(ksp));
+    PetscCallAbort(comm, KSPSolve(ksp, b, x));
+
+    {
+        VecSpan xdata{x};
+        ASSERT_NEAR( 0.75, xdata[0], 1e-8);
+        ASSERT_NEAR( 0.25, xdata[1], 1e-8);
+        ASSERT_NEAR(-0.25, xdata[2], 1e-8);
+        ASSERT_NEAR( 0.45, xdata[3], 1e-8);
+        ASSERT_NEAR(-0.20, xdata[4], 1e-8);
+        ASSERT_NEAR( 0.75, xdata[5], 1e-8);
     }
-
-    /* Destory solver and preconditioner */
-    HYPRE_ParCSRFlexGMRESDestroy(solver);
-    HYPRE_BoomerAMGDestroy(precond);
-
-    std::vector<double> xdata(6);
-    xvec.extract_data(std::span{xdata});
-//    ASSERT_DOUBLE_EQ( 0.75, xdata[0]);
-//    ASSERT_DOUBLE_EQ( 0.25, xdata[1]);
-//    ASSERT_DOUBLE_EQ(-0.25, xdata[2]);
-//    ASSERT_DOUBLE_EQ( 0.45, xdata[3]);
-//    ASSERT_DOUBLE_EQ(-0.20, xdata[4]);
-//    ASSERT_DOUBLE_EQ( 0.75, xdata[5]);
+    
+    // cleanup
+    KSPDestroy(&ksp);
+    MatDestroy(&A);
+    VecDestroy(&b);
+    VecDestroy(&x);
 }
