ca96f596068388bbd3a92eb36e3d18f426d3c51c
Gianni Absillis <gianniabsillis@gmail.com>
2024-04-25 19:34:21 +0000

    fix boilerplate in conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -49,8 +49,8 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 breathe_projects = {}
 
 if read_the_docs_build:
-    input_dir = '../CatCutifier'
+    input_dir = '../src'
     output_dir = 'build'
     configureDoxyfile(input_dir, output_dir)
     subprocess.call('doxygen', shell=True)
-    breathe_projects['CatCutifier'] = output_dir + '/xml'
+    breathe_projects['ICEicle'] = output_dir + '/xml'
